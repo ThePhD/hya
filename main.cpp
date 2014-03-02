c09a567f2c66492201cc8b15ecb94149613e6fd4
ThePhD <phdofthehouse@gmail.com>
2014-03-02 15:26:48 -0800

    Initial setup for hya.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Furrovine++/Graphics/Window.h>
+#include <Furrovine++/WindowDriver.h>
+#include <Furrovine++/Audio/AudioDevice.h>
+
+int main ( ) {
+	using namespace Furrovine;
+	using namespace Furrovine::Graphics;
+	using namespace Furrovine::Audio;
+	
+	AudioDevice audiodevice( 16, 2, 44100 );
+
+}

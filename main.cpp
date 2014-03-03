30c8cc69b8a427eff98fa86b90e57dc4156b9dc3
ThePhD <phdofthehouse@gmail.com>
2014-03-03 10:35:02 -0500

    Getting ready to create a sine wave continuously to produce note.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <Furrovine++/Graphics/Window.h>
 #include <Furrovine++/WindowDriver.h>
 #include <Furrovine++/Audio/AudioDevice.h>
+#include <Furrovine++/Stopwatch.h>
 
 int main ( ) {
 	using namespace Furrovine;
 	using namespace Furrovine::Graphics;
 	using namespace Furrovine::Audio;
 	
-	AudioDevice audiodevice( 16, 2, 44100 );
+	uint32 samplerate = 44100;
+	uint32 bitspersample = 16;
+	uint32 channelcount = 2;
+	AudioDevice audiodevice( bitspersample, channelcount, samplerate );
+	
+	std::vector<uint16> bufferdata( samplerate * 2 );
+	AudioBuffer buffer( reinterpret_cast<byte*>( bufferdata.data() ), size_as<byte, uint16>( samplerate ), 0, samplerate );
+	
+	Stopwatch stopwatch;
+	stopwatch.Start( );
+	while ( true ) {
+		double seconds = stopwatch.ElapsedSeconds( );
+
+		if ( seconds < 1 )
+			continue;
+		audiodevice.SubmitBuffer( buffer );
+		stopwatch.Restart( );
+	}
 
 }
